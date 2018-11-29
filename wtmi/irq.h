12bce1bb36f6554617efa1b26dc0ff347b20aec2
Marek Behún <marek.behun@nic.cz>
2018-11-29 19:55:52 +0100

    Fix header function name
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- wtmi/irq.h
+++ wtmi/irq.h
@@ -37,7 +37,7 @@ typedef void (*irq_handler_t)(int);
 
 extern void register_irq_handler(int irq, irq_handler_t handler);
 extern void enable_systick(void);
-extern void enable_systick(void);
+extern void disable_systick(void);
 
 static inline void enable_irq(void)
 {
