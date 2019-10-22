a2707a5da99612d545fe11cac72233ddae82c843
Marek Behún <marek.behun@nic.cz>
2019-10-23 01:22:34 +0200

    Uninline string functions
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

---
+++ wtmi/string.c
@@ -0,0 +1,81 @@
+#include "string.h"
+
+void *memset(void *dest, int c, u32 n)
+{
+	u8 *d = dest;
+
+	while (n--)
+		*d++ = c;
+
+	return dest;
+}
+
+void *memcpy(void *dest, const void *src, u32 n)
+{
+	u8 *d = dest, *e = d + n;
+	const u8 *s = src;
+
+	while (d < e)
+		*d++ = *s++;
+
+	return dest;
+}
+
+void *memmove(void *dest, const void *src, u32 n)
+{
+	if (dest == src) {
+		return dest;
+	} else if (dest < src || src + n <= dest) {
+		return memcpy(dest, src, n);
+	} else {
+		u8 *d = dest + n, *e = dest;
+		const u8 *s = src + n;
+
+		while (d > e)
+			*d-- = *s--;
+
+		return dest;
+	}
+}
+
+int strcmp(const char *p1, const char *p2)
+{
+	while (*p1 == *p2) {
+		if (*p1 == '\0')
+			return 0;
+		++p1, ++p2;
+	}
+
+	return *p1 - *p2;
+}
+
+int strncmp(const char *p1, const char *p2, u32 n)
+{
+	while (n && *p1 == *p2) {
+		if (*p1 == '\0')
+			return 0;
+		++p1, ++p2, --n;
+	}
+
+	return n ? (*p1 - *p2) : 0;
+}
+
+u32 strlen(const char *s)
+{
+	u32 res = 0;
+
+	while (*s++)
+		++res;
+
+	return res;
+}
+
+u32 strnlen(const char *s, u32 m)
+{
+	u32 res = 0;
+
+	while (*s++ && m)
+		++res, --m;
+
+	return res;
+}
