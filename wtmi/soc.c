74c0329bac41beeeeb5a5c25059a6e98c835c771
Marek Behún <marek.behun@nic.cz>
2019-10-31 05:23:34 +0100

    wtmi: some more work on reset
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- wtmi/soc.c
+++ wtmi/soc.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "io.h"
+#include "clock.h"
 #include "irq.h"
 #include "mbox.h"
 #include "spi.h"
@@ -8,8 +9,247 @@
 #include "gpio.h"
 #include "printf.h"
 #include "debug.h"
+#include "tim.h"
 
-#if 0
+#define NB_RESET		0xc0012400
+#define SB_RESET		0xc0018600
+
+#define INDIR_ADDR		0xc00e0178
+#define INDIR_DATA		0xc00e017c
+
+struct reg_val {
+	u32 reg;
+	u32 val;
+};
+
+static void write_indirect(u32 val, u32 addr)
+{
+	writel(addr, INDIR_ADDR);
+	writel(val, INDIR_DATA);
+}
+
+static void _write_reg_vals(const struct reg_val *p, u32 n)
+{
+	while (n) {
+		writel(p->val, p->reg);
+		++p, --n;
+	}
+}
+
+#define write_reg_vals(x) _write_reg_vals((x), sizeof((x))/sizeof((x)[0]))
+
+static const struct reg_val reset_nb_regs[] = {
+	/* UART 1 default values */
+	{ 0xc0012008, 0x00042000 },
+	{ 0xc0012010, 0x0002c807 },
+	{ 0xc0012014, 0x1e1e1e1e },
+
+	/* NB Clocks */
+	{ 0xc0013000, 0x03cfccf2 },
+	{ 0xc0013004, 0x1296202c },
+	{ 0xc0013008, 0x21061aa9 },
+	{ 0xc001300c, 0x20543084 },
+	{ 0xc0013010, 0x00009fff },
+	{ 0xc0013014, 0x00000000 },
+
+	/* NB GPIO */
+	{ 0xc0013800, 0 },
+	{ 0xc0013804, 0 },
+	{ 0xc0013818, 0 },
+	{ 0xc001381c, 0 },
+	{ 0xc0013830, 0x000d17fa },
+	{ 0xc0013c00, 0 },
+	{ 0xc0013c04, 0 },
+	{ 0xc0013c08, 0 },
+	{ 0xc0013c10, 0xffffffff },
+	{ 0xc0013c14, 0xf },
+	{ 0xc0013c18, 0 },
+	{ 0xc0013c1c, 0 },
+
+	/* NB PWM */
+	{ 0xc0015200, 0 },
+	{ 0xc0015204, 0 },
+	{ 0xc0015210, 0 },
+	{ 0xc0015214, 0 },
+	{ 0xc0015220, 0 },
+	{ 0xc0015224, 0 },
+	{ 0xc0015230, 0 },
+	{ 0xc0015234, 0 },
+	{ 0xc0015240, 0 },
+
+	/* NB Pad Control */
+	{ 0xc0015600, 0x00007eaa },
+	{ 0xc0015604, 0x00e84961 },
+	{ 0xc0015608, 0x00007800 },
+	{ 0xc001560c, 0x000f83ff },
+
+	/* NB Misc */
+	{ 0xc0017808, 0 },
+};
+
+static const struct reg_val reset_sb_regs[] = {
+	/* SB Clocks */
+	{ 0xc0018000, 0x003f8f40 },
+	{ 0xc0018004, 0x02515508 },
+	{ 0xc0018008, 0x00300880 },
+	{ 0xc001800c, 0x00000540 },
+	{ 0xc0018010, 0x000007aa },
+	{ 0xc0018014, 0x00180000 },
+
+	/* SB PHY */
+	{ 0xc0018300, 0x00071800 },
+	{ 0xc001830c, 0x0000000e },
+	{ 0xc0018310, 0 },
+	{ 0xc0018328, 0x00071800 },
+	{ 0xc0018334, 0x0000000e },
+	{ 0xc0018338, 0 },
+	{ 0xc00183fc, 0x00000011 },
+
+	/* SB GPIO */
+	{ 0xc0018800, 0 },
+	{ 0xc0018818, 0 },
+	{ 0xc0018830, 0x7e3b },
+	{ 0xc0018c00, 0 },
+	{ 0xc0018c08, 0 },
+	{ 0xc0018c10, 0xffffffff },
+	{ 0xc0018c18, 0 },
+
+	/* SB Pad Control */
+	{ 0xc001a400, 0x775a },
+	{ 0xc001a404, 0x3043 },
+	{ 0xc001a408, 0x003c },
+
+	/* SB Misc */
+	{ 0xc001e808, 0 },
+	{ 0xc001e80c, 0x01000031 },
+	{ 0xc001e810, 1 },
+	{ 0xc001e814, 0xe7fffff0 },
+	{ 0xc001e818, 0 },
+	{ 0xc001e81c, 0x3f },
+};
+
+static const struct reg_val reset_soc_irqs_regs[] = {
+	{ 0xc0008a04, 0xffffffff },
+	{ 0xc0008a08, 0xffffffff },
+	{ 0xc0008a0c, 0 },
+	{ 0xc0008a00, 0 },
+	{ 0xc0008a14, 0xffffffff },
+	{ 0xc0008a18, 0xffffffff },
+	{ 0xc0008a1c, 0 },
+	{ 0xc0008a10, 0 },
+};
+
+static const struct reg_val reset_wdt_and_counters_regs[] = {
+	{ 0xc000d064, 0 },
+	{ 0xc0008300, 0x200 },
+	{ 0xc0008310, 0x200 },
+	{ 0xc0008320, 0x200 },
+	{ 0xc0008330, 0x204 },
+	{ 0xc0008330, 0x205 },
+};
+
+static const struct reg_val reset_a53_regs[] = {
+	/* CPU SMPEN */
+	{ 0xc000d010, BIT(14) },
+	/* CPU Configuration */
+	{ 0xc000d00c, 0x66ffc030 },
+	/* Reset ATF Mailbox */
+	{ 0x64000400, 0 },
+};
+
+static void write_range(u32 start, u32 end, u32 value)
+{
+	u32 reg;
+
+	for (reg = start; reg < end; reg += 4)
+		writel(value, reg);
+}
+
+static void reset_soc_irqs(void)
+{
+	write_reg_vals(reset_soc_irqs_regs);
+
+	write_range(0xc0008a30, 0xc0008a64, 0);
+	writel(0, 0xc1d00000);
+	write_range(0xc1d00084, 0xc1d000fc, 0);
+	write_range(0xc1d00184, 0xc1d001fc, 0xffffffff);
+	write_range(0xc1d00284, 0xc1d001fc, 0xffffffff);
+	write_range(0xc1d00384, 0xc1d001fc, 0xffffffff);
+}
+
+#ifdef DEBUG_UART2
+#define NB_RESET_UART2		BIT(6)
+#else
+#define NB_RESET_UART2		0
+#endif
+
+static void reset_peripherals(void)
+{
+	/* disable PHY */
+	mdio_write(1, 22, 0);
+	mdio_write(1, 0, BIT(15));
+
+	/* North Bridge peripherals reset */
+	writel(BIT(10) | BIT(3) | NB_RESET_UART2, NB_RESET);
+	wait_ns(1000000);
+	writel(0x7fcff, NB_RESET);
+	wait_ns(1000000);
+
+	write_reg_vals(reset_nb_regs);
+
+	/* South Bridge peripheral reset */
+	writel(0, SB_RESET);
+	wait_ns(1000000);
+	writel(0xf3c, SB_RESET);
+	wait_ns(1000000);
+
+	write_reg_vals(reset_sb_regs);
+
+	/* PCIE/GBE0 PHY */
+	writel(0x122, 0xc001f382);
+	wait_ns(10000);
+	writel(0x21, 0xc001f382);
+	wait_ns(10000);
+
+	/* USB3/GBE1 PHY */
+	writel(0x122, 0xc005c382);
+	wait_ns(10000);
+	writel(0x21, 0xc005c382);
+	wait_ns(10000);
+
+	/* USB3/SATA PHY */
+	write_indirect(0x122, 0x3c1);
+	wait_ns(10000);
+	write_indirect(0x21, 0x3c1);
+	wait_ns(10000);
+}
+
+static void cpu_software_reset(void)
+{
+	writel(0xa1b2c3d4, 0xc000d060);
+}
+
+static void reset_soc(void)
+{
+	/* swrst; kick h; reset; kick */
+
+	cpu_software_reset();
+	write_reg_vals(reset_a53_regs);
+	reset_soc_irqs();
+	write_reg_vals(reset_wdt_and_counters_regs);
+	reset_peripherals();
+
+	load_image(OBMI_ID, (void *)(AP_RAM + ATF_ENTRY_ADDRESS), NULL);
+	cpu_software_reset();
+}
+
+DECL_DEBUG_CMD(reset)
+{
+	reset_soc();
+}
+
+DEBUG_CMD("reset", "Reset SOC and peripherals", reset);
+
 #include "a53_helper/a53_helper.c"
 
 DECL_DEBUG_CMD(kick)
@@ -17,19 +257,14 @@ DECL_DEBUG_CMD(kick)
 	u32 addr = ATF_ENTRY_ADDRESS;
 	enum { None, Helper, Uboot } load = None;
 
-	if (argc > 1 && number(argv[1], &addr)) {
+	if (argc > 1) {
 		if (argv[1][0] == 'h') {
 			load = Helper;
 		} else if (argv[1][0] == 'u') {
 			load = Uboot;
-		} else {
+		} else if (number(argv[1], &addr)) {
 			goto usage;
 		}
-		++argv;
-		--argc;
-
-		if (argc > 1 && number(argv[1], &addr))
-			return;
 	}
 
 	switch (load) {
@@ -38,6 +273,7 @@ DECL_DEBUG_CMD(kick)
 		       sizeof(a53_helper_code));
 		break;
 	case Uboot:
+		spi_init(&nordev);
 		spi_nor_read(&nordev, (void *)(AP_RAM + addr), 0x20000,
 			     0x160000);
 		break;
@@ -55,56 +291,6 @@ usage:
 }
 
 DEBUG_CMD("kick", "Kick AP", kick);
-
-DECL_DEBUG_CMD(swrst)
-{
-	u32 reg;
-
-	mdio_write(1, 22, 0);
-	mdio_write(1, 0, BIT(15));
-
-	writel(0xa1b2c3d4, 0xc000d060);
-	writel(0x4000, 0xc000d010);
-	writel(0x66ffc030, 0xc000d00c);
-//	setbitsl(0xc000d00c, 0, BIT(31));
-
-	writel(0xffffffff, 0xc0008a04);
-	writel(0xffffffff, 0xc0008a08);
-	writel(0, 0xc0008a0c);
-	writel(0, 0xc0008a00);
-
-	writel(0xffffffff, 0xc0008a14);
-	writel(0xffffffff, 0xc0008a18);
-	writel(0xf003c000, 0xc0008a1c);
-	writel(0, 0xc0008a10);
-
-	for (reg = 0xc0008a30; reg < 0xc0008a64; reg += 4)
-		writel(0, reg);
-
-	writel(0, 0xc000d064);
-	writel(0x0200, 0xc0008300);
-	writel(0x0200, 0xc0008310);
-	writel(0x0200, 0xc0008320);
-	writel(0x0204, 0xc0008330);
-	writel(0x0205, 0xc0008330);
-
-	/* clocks to default values */
-	writel(0x03cfccf2, 0xc0013000);
-	writel(0x1296202c, 0xc0013004);
-	writel(0x21061aa9, 0xc0013008);
-	writel(0x20543084, 0xc001300c);
-	writel(0x00009fff, 0xc0013010);
-	writel(0x00000000, 0xc0013014);
-	writel(0x003f8f40, 0xc0018000);
-	writel(0x02515508, 0xc0018004);
-	writel(0x00300880, 0xc0018008);
-	writel(0x00000540, 0xc001800c);
-	writel(0x000007aa, 0xc0018010);
-	writel(0x00180000, 0xc0018014);
-}
-
-DEBUG_CMD("swrst", "CPU Software Reset", swrst);
-#endif
 
 DECL_DEBUG_CMD(info)
 {
@@ -129,17 +315,13 @@ DECL_DEBUG_CMD(info)
 		cntr /= 25000;
 		printf("Counter value: %d ms\n", (u32)cntr);
 	}
-
-#define l(a) printf(#a " = %08x\n", readl((a)))
-
-	l(0xc000d00c);
-	l(0xc000d010);
-	l(0xc000d044);
-	l(0xc000d048);
-	l(0xc000d0a0);
-	l(0xc000d0a4);
-	l(0xc000d0b0);
-	l(0xc000d0c0);
 }
 
 DEBUG_CMD("info", "Show some CPU info", info);
+
+DECL_DEBUG_CMD(swrst)
+{
+	cpu_software_reset();
+}
+
+DEBUG_CMD("swrst", "CPU Software Reset", swrst);
