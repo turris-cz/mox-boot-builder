fa03df9b382278737bcc78a2e178904a79279fe2
Marek Behún <marek.behun@nic.cz>
2020-01-21 21:11:16 +0100

    wtmi: a53_helper: update types.h
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- wtmi/a53_helper/types.h
+++ wtmi/a53_helper/types.h
@@ -5,5 +5,12 @@ typedef unsigned long long u_register_t;
 typedef unsigned long long size_t;
 typedef unsigned long long u64;
 typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+#define NULL ((void *)0)
+
+#define MIN(a, b) ({ __auto_type _a = (a); __auto_type _b = (b); _a < _b ? _a : _b; })
+#define MAX(a, b) ({ __auto_type _a = (a); __auto_type _b = (b); _a > _b ? _a : _b; })
 
 #endif /* !__TYPES_H__ */
