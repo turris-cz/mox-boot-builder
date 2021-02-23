19681b1d3aada9a106ee1524a0d023805b1162be
Marek Behún <marek.behun@nic.cz>
2021-02-23 22:43:10 +0100

    wtmi: mdio: enable SMI pinctrl and add mii debug command
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- wtmi/mdio.c
+++ wtmi/mdio.c
@@ -2,6 +2,7 @@
 #include "io.h"
 #include "clock.h"
 #include "mdio.h"
+#include "debug.h"
 
 #define SMI		0xc0032004
 #define SMI_BUSY	BIT(28)
@@ -16,10 +17,29 @@ static inline void mdio_wait(void)
 		ndelay(100);
 }
 
+static int mdio_pins_disable;
+
+void mdio_begin(void)
+{
+	if (readl(SB_PINCTRL) & BIT(4)) {
+		setbitsl(SB_PINCTRL, 0, BIT(4));
+		mdio_pins_disable = 1;
+	}
+}
+
+void mdio_end(void)
+{
+	if (mdio_pins_disable) {
+		setbitsl(SB_PINCTRL, BIT(4), BIT(4));
+		mdio_pins_disable = 0;
+	}
+}
+
 void mdio_write(int dev, int reg, u16 val)
 {
 	mdio_wait();
 	writel(SMI_DEV(dev) | SMI_REG(reg) | val, SMI);
+	mdio_wait();
 }
 
 int mdio_read(int dev, int reg)
@@ -28,11 +48,52 @@ int mdio_read(int dev, int reg)
 
 	mdio_wait();
 	writel(SMI_DEV(dev) | SMI_REG(reg) | SMI_READ, SMI);
-	mdio_wait();
 
+	mdio_wait();
 	val = readl(SMI);
+
 	if (val & SMI_READ_VALID)
 		return val & 0xffff;
 	else
 		return -1;
 }
+
+DECL_DEBUG_CMD(cmd_mii)
+{
+	u32 addr, reg;
+
+	if (argc < 4 || number(argv[2], &addr) || number(argv[3], &reg) ||
+	    addr > 0x1f || reg > 0x1f)
+		goto usage;
+
+	if (argv[1][0] == 'r') {
+		int res;
+
+		mdio_begin();
+		res = mdio_read(addr, reg);
+		mdio_end();
+
+		if (res < 0)
+			printf("Cannot read at address %#04x!\n", addr);
+		else
+			printf("%#06x\n", res);
+	} else if (argv[1][0] == 'w') {
+		u32 val;
+
+		if (argc < 5 || number(argv[4], &val) || val > 0xffff)
+			goto usage;
+
+		mdio_begin();
+		mdio_write(addr, reg, val);
+		mdio_end();
+	} else {
+		goto usage;
+	}
+
+	return;
+usage:
+	printf("usage: mii read <addr> <reg>\n"
+	       "       mii write <addr> <reg> <data>\n");
+}
+
+DEBUG_CMD("mii", "raw MII access", cmd_mii);
--- wtmi/mdio.h
+++ wtmi/mdio.h
@@ -3,7 +3,9 @@
 
 #include "types.h"
 
-void mdio_write(int dev, int reg, u16 val);
-int mdio_read(int dev, int reg);
+extern void mdio_begin(void);
+extern void mdio_end(void);
+extern void mdio_write(int dev, int reg, u16 val);
+extern int mdio_read(int dev, int reg);
 
 #endif /* _MDIO_H_ */
