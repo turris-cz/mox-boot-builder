5127638ae30cdc186d25aed136e84b20d7471ed3
Marek Behún <marek.behun@nic.cz>
2021-10-07 11:42:44 +0200

    wtmi: Use the signed keyword when defining signed types
    The char type is implicitly unsigned on ARM.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- wtmi/types.h
+++ wtmi/types.h
@@ -19,23 +19,23 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-typedef char			int_8;
+typedef signed char		int_8;
 typedef unsigned char		uint_8;
-typedef short			int_16;
+typedef signed short		int_16;
 typedef unsigned short		uint_16;
-typedef int			int_32;
+typedef signed int		int_32;
 typedef unsigned int		uint_32;
-typedef long long		int_64;
+typedef signed long long	int_64;
 typedef unsigned long long	uint_64;
 
 typedef unsigned long long	u64;
 typedef unsigned int		u32;
 typedef unsigned short		u16;
 typedef unsigned char		u8;
-typedef long long		s64;
-typedef int			s32;
-typedef short			s16;
-typedef char			s8;
+typedef signed long long	s64;
+typedef signed int		s32;
+typedef signed short		s16;
+typedef signed char		s8;
 typedef char			byte;
 
 typedef u32			size_t;
