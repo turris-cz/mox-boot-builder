turris-cz/mox-boot-builder

commit 3dac4feba60b026cd0e9a6e1ed29cc3c32ca5e1e

wtmi: Fix detection of Armada 3720 Devel Board
Armada 3720 Devel Board has only one 88E1512 PHY, either at MDIO address
0x00 (in SGMII mode), or at address 0x01 (in RGMII mode).

Signed-off-by: Pali Rohár <pali@kernel.org>
Signed-off-by: Marek Behún <marek.behun@nic.cz>

wtmi/board.c
@@ -39,7 +39,8 @@ static enum board _get_board(void)
 
 	if (reg0 == 0xffff && reg1 == 0xffff) {
 		return uDPU;
-	} else if (reg0 == 0x0141 && reg1 == 0x0141) {
+	} else if ((reg0 == 0x0141 && reg1 == 0xffff) ||
+		   (reg0 == 0xffff && reg1 == 0x0141)) {
 		return Armada3720_DB;
 	} else if (reg0 == 0xffff && reg1 == 0x0141) {
 		u8 id[6];

commit 54cf2348f5c3912a9b80847f6eadaa7db7f300f2

wtmi: Fix detection of some boards
Fix detection of Turris devices without OTP data burned, and that of
ESPRESSObin Ultra.

This was broken in commit 3dac4feba60b ("wtmi: Fix detection of Armada
3720 Devel Board").

Signed-off-by: Marek Behún <marek.behun@nic.cz>

wtmi/board.c
@@ -18,14 +18,21 @@ static enum board _get_board_fallback(void)
 
 	if (reg03 == 0xffff && reg13 == 0xffff) {
 		return uDPU;
-	} else if ((reg02 == 0x0141 && reg13 == 0xffff) ||
-		   (reg03 == 0xffff && reg12 == 0x0141)) {
+	} else if (reg02 == 0x0141 && reg13 == 0xffff) {
 		return Armada3720_DB;
 	} else if (reg03 == 0xffff && reg12 == 0x0141) {
+		int id151x;
 		u8 id[6];
 
 		if (reg33 != 0xffff && reg32 == reg33)
 			return ESPRESSObin_Ultra;
+
+		mdio_write(1, 22, 18);
+		id151x = mdio_read(1, 30);
+		mdio_write(1, 22, 0);
+
+		if (id151x == 0x0006)
+			return Armada3720_DB;
 
 		spi_init(&nordev);
 		spi_nor_read_id(&nordev, id);